b716703253b643e53f9d94b7ad0c1af8ec0cc875
axelfayet97 <axel.gayet97@gmail.com>
2022-03-17 18:01:16 +0100

    Updated package & package lock

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 const { json } = require('express');
 const express = require('express');
 const mongoose = require('mongoose');
-
-const Thing = require('./models/Thing')
+const bodyParser = require('body-parser');
+const Thing = require('./models/Thing');
 
 mongoose.connect('mongodb+srv://axel_fayet:password1234@cluster0.ycqur.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -33,53 +33,21 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 
 // Route post pour envoi d'objet
-app.post('api/stuff', (req, res, next) => {
-    // Pas besoin de l'id
+app.post('/api/stuff', (req, res, next) => {
     delete req.body._id;
-    // déclaration d'un nouvel objet
     const thing = new Thing({
-        // Copie des champs présents dans le coprs de la requête avec l'opérateur Spread
         ...req.body
-        // == req.body.title, req.body.description, .....
     });
-    // Enregistrement dans la base de données
     thing.save()
         .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
         .catch(error => res.status(400).json({ error }));
-    // next ?
-    next();
-});
-
-//intercepte requetes post
-app.post('/api/stuff', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-        message: 'Objet créé.'
-    });
 });
 
 // Middleware contenant un tableau de nos objets // Pour route GET
-app.get('/api/stuff', (req, res, next) => {
-    const stuff = [
-        {
-            _id: 'oeihfzeoi',
-            title: 'Mon premier objet',
-            description: 'Les infos de mon premier objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 4900,
-            userId: 'qsomihvqios',
-        },
-        {
-            _id: 'oeihfzeomoihi',
-            title: 'Mon deuxième objet',
-            description: 'Les infos de mon deuxième objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 2900,
-            userId: 'qsomihvqios',
-        },
-    ];
-    // Réponse du serveur avec un code 200, renvoie aussi le tableau des objets
-    res.status(200).json(stuff);
+app.use('/api/stuff', (req, res, next) => {
+    Thing.find()
+        .then(things => res.status(200).json(things))
+        .catch(error => res.status(400).json({ error }));
 });
 
 // Export de l'application express
