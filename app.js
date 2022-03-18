72427d82c898d28ad4aa4ab7c966442928d76362
axelfayet97 <axel.gayet97@gmail.com>
2022-03-18 09:15:57 +0100

    Created put route for item modification, get route to display single page, and delete route to delete an item

--- app.js
+++ app.js
@@ -47,8 +47,29 @@ app.post('/api/stuff', (req, res, next) => {
         .catch(error => res.status(400).json({ error }));
 });
 
+// Modification d'un objet
+app.put('/api/stuff/:id', (req, res, next) => {
+    Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+        .then(() => res.status(200).json({ message: "OK" }))
+        .catch(error => res.status(400).json({ error }))
+});
+
+// Suppression d'un objet
+app.delete('/api/stuff/:id', (req, res, next) => {
+    Thing.deleteOne({ _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
+        .catch(error => res.status(400).json({ error }));
+});
+
+// Récupération d'un Thing spécifique
+app.get('/api/stuff/:id', (req, res, next) => {
+    Thing.findOne({ _id: req.params.id })
+        .then(thing => res.status(200).json(thing))
+        .catch(error => res.status(404).json({ error }));
+});
+
 // Récupération du tableau things a l'aide de la méthode find()
-app.use('/api/stuff', (req, res, next) => {
+app.get('/api/stuff', (req, res, next) => {
     Thing.find()
         .then(things => res.status(200).json(things))
         .catch(error => res.status(400).json({ error }));
