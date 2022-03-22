ea0e03cae3dfada146b45056103129fb37e7734d
axelfayet97 <axel.gayet97@gmail.com>
2022-03-22 11:30:14 +0100

    Re installed bcrypt, there was an issue with the ancient package

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,5 @@
 // Controleurs d'authentification
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 
 // Inscription
@@ -27,16 +27,16 @@ exports.login = (req, res, next) => {
         .then(user => {
             // Si nom utilisateur inexistant
             if (!user) {
-                return res.status(401).json({ error: 'Utilisateur non trouve !' });
+                return res.status(401).json({ error: 'Utilisateur non trouvé !' });
             }
             // Sinon, on compare le pwd saisi avec le hash contenu dans la BDD
-            bcrypt.compare(req.body.json, user.password)
+            bcrypt.compare(req.body.password, user.password)
                 .then(valid => {
                     // Si invalide
                     if (!valid) {
                         return res.status(401).json({ error: 'Mot de passe incorrect' });
                     }
-                    // Sinon, création d'un objet JSON contenan l'id et un token
+                    // Sinon, création d'un objet JSON contenant l'id et un token
                     res.status(200).json({
                         userId: user._id,
                         token: 'TOKEN'
