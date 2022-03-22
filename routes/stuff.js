635bfdf1d96023b71ed4ddb47102a72cd44b78d4
axelfayet97 <axel.gayet97@gmail.com>
2022-03-22 10:01:41 +0100

    imported controlers from stuff controler

--- routes/stuff.js
+++ routes/stuff.js
@@ -1,48 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const Thing = require('../models/thing');
+const stuffCtrl = require('../controlers/stuff');
 
 // Route post pour envoi d'objet
-router.post('/', (req, res, next) => {
-    // Pas besoin de l'id
-    delete req.body._id;
-    // Nouvel objet Thing
-    const thing = new Thing({
-        // Opérateur spread donnant tous les champs du body
-        ...req.body
-    });
-    // Methode save dans la bdd
-    thing.save()
-        .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
-        .catch(error => res.status(400).json({ error }));
-});
-
+router.post('/', stuffCtrl.createThing);
 // Modification d'un objet
-router.put('/:id', (req, res, next) => {
-    Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-        .then(() => res.status(200).json({ message: "OK" }))
-        .catch(error => res.status(400).json({ error }))
-});
-
+router.put('/:id', stuffCtrl.modifyThing);
 // Suppression d'un objet
-router.delete('/:id', (req, res, next) => {
-    Thing.deleteOne({ _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
-        .catch(error => res.status(400).json({ error }));
-});
-
+router.delete('/:id', stuffCtrl.deleteThing);
 // Récupération d'un Thing spécifique
-router.get('/:id', (req, res, next) => {
-    Thing.findOne({ _id: req.params.id })
-        .then(thing => res.status(200).json(thing))
-        .catch(error => res.status(404).json({ error }));
-});
-
+router.get('/:id', stuffCtrl.getOneThing);
 // Récupération du tableau things a l'aide de la méthode find()
-router.get('/', (req, res, next) => {
-    Thing.find()
-        .then(things => res.status(200).json(things))
-        .catch(error => res.status(400).json({ error }));
-});
+router.get('/', stuffCtrl.getAllThings);
 
 module.exports = router;
