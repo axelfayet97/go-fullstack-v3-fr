7f876b72e0ed9938f5c8346aac3eac93301942a7
axelfayet97 <axel.gayet97@gmail.com>
2022-03-23 11:45:10 +0100

    Created authentification middleware

---
+++ middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+
+
+module.exports = (req, res, next) => {
+    try {
+        // Récupération du token dans le header authorization
+        const token = req.headers.authorization.split(' ')[1];
+        // Décoder le token avec la clé secrète
+        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+        const userId = decodedToken.userId;
+        // Si userID différent du userID dans le body de la requete, alors erreur
+        if (req.body.userId && req.body.userId !== userId) {
+            throw 'User ID non valable !';
+        } else {
+            // Requête réussie, passage au prochain middleware
+            next();
+        }
+    } catch (error) {
+        // Sinon catch erreur
+        res.status(401).json({ error: error || 'Requête non authentifiée !' });
+    };
+};
